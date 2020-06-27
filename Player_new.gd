b8872af035118277d752aa44ab4c5d7d523cd2b8
envyniv <42321092+envyniv@users.noreply.github.com>
2020-06-27 12:24:33 +0200

    check desc 27/06/20
    added chromaticaberration shader, fixed up minor stuff in quinton's sprite sheet, edited the design document, tweaked the player script, removed white background on the title image

--- Player_new.gd
+++ Player_new.gd
@@ -1,7 +1,26 @@
 extends KinematicBody2D
 
 #initialize
+var right = Input.is_action_pressed("ui_right")
+var left = Input.is_action_pressed("ui_left")
+var up = Input.is_action_pressed("ui_up")
+var down = Input.is_action_pressed("ui_down")
+var speed = 100
+var velocity = Vector2()
 
+#get input
+func get_input():
+	if right:
+		velocity.x += 1
+	if left:
+		velocity.x -= 1
+	if down:
+		velocity.y += 1
+	if up:
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+	pass
+	
 #state machine
 enum {
 	STILL
@@ -11,39 +30,13 @@ enum {
 }
 
 var state = STILL
-var moveSpeed = 100
-var motion = Vector2()
 
 # movement
 func _process(delta):
-	motion.x = 0; motion.y = 0
-
-	if Input.is_action_pressed("ui_right"):
-		motion.x = moveSpeed
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -moveSpeed
-	elif Input.is_action_pressed("ui_up"):
-		motion.y = -moveSpeed
-	elif Input.is_action_pressed("ui_down"):
-		motion.y = moveSpeed
-	motion = move_and_slide(motion)
-
+	get_input()
+	move_and_collide(velocity * delta)
 	pass
 
 func anim_handle(): #'you should probably check if the player *is* moving, rather than if they *aren't*' -gotimo2
-	if motion.x == 0 and motion.y == 0:
-		$Sprite/AnimationPlayer.play("idle")
-		
-		#needs to be expanded
-		
-	elif motion.x != 0:
-		if motion.x < 0:
-			$Sprite/AnimationPlayer.play("walk_a")
-		if motion.x > 0:
-			$Sprite/AnimationPlayer.play("walk_d")
-	elif motion.y != 0:
-		if motion.y < 0:
-			$Sprite/AnimationPlayer.play("walk_w")
-		if motion.y > 0:
-			$Sprite/AnimationPlayer.play("walk_s")
+# 
 	pass
