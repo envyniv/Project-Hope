0b0fab8a64b43fa902f4872c8c4691fd3ea5735a
envyniv <42321092+envyniv@users.noreply.github.com>
2020-07-16 16:51:18 +0200

    implemented functioning state machine

--- scripts/Player/Player.gd
+++ scripts/Player/Player.gd
@@ -1,49 +1,68 @@
 extends KinematicBody2D
 
-#initialize
-var moveSpeed = 100
+#init
+var moveSpeed = 75
 var movedir = Vector2.ZERO
 var canDo = true #used for special actions like attacking and dodging
 onready var animPlayer=$Sprite/AnimationPlayer
 onready var animTree=$Sprite/AnimationPlayer/AnimationTree
 onready var animState=animTree.get("parameters/playback")
-
+#end_init
 	
-#state machine
+#state
 enum {
-	STILL
-	MOVING
-	RUNNING
-	SPECIAL
-	MINIGM
+	MOVE,
+	RUN,
+	ATK,
+	DODGE,
 }
 
-var state = STILL
+var state = null
+#end_state
+
+func _ready():
+	animTree.active = true
+	pass
+
 
 func _physics_process(_delta):
-	input_check();
-	movement();
-	anim_handler();
+	inputChk();
+	animHndlr();
+	#moving
+	match state:
+		MOVE:
+			moveState();
+		RUN:
+			runState();
+		ATK:
+			atkState();
+		DODGE:
+			dodgeState();
+			
+	if movedir!=Vector2.ZERO:
+		state=MOVE;
 	pass
 
 #input
-func input_check():
+func inputChk():
 	var UP = Input.is_action_pressed("ui_up")
 	var DOWN = Input.is_action_pressed("ui_down")
 	var RIGHT = Input.is_action_pressed("ui_right")
 	var LEFT = Input.is_action_pressed("ui_left")
-	movedir.x = -int(LEFT) + int(RIGHT) #made so that when pressing u&d or l&r the player won't have a stroke.
+	var ATTACK = Input.is_action_just_pressed("ui_attack")
+	movedir.x = -int(LEFT) + int(RIGHT) #don't move if the left and right keys are pressed
 	movedir.y = -int(UP) + int(DOWN)
 	pass
 
 
-# movement
-func movement():
-	var motion = movedir.normalized() * moveSpeed
-	move_and_slide(motion, Vector2(0,0))
-	pass
 
-func anim_handler(): #'you should probably check if the player *is* moving, rather than if they *aren't*' -gotimo2
+#func moving():
+#	var motion = movedir.normalized() * moveSpeed
+#	move_and_slide(motion, Vector2(0,0))
+#	pass
+
+#animation
+func animHndlr():
 	if movedir!=Vector2.ZERO:
 		animTree.set("parameters/idle/blend_position", movedir)
 		animTree.set("parameters/atk/blend_position", movedir)
@@ -52,5 +71,28 @@ func anim_handler(): #'you should probably check if the player *is* moving, rath
 		animTree.set("parameters/walk/blend_position", movedir)
 		animState.travel("walk")
 		pass
+	else:
+		animState.travel("idle")
+		pass
+	
+	pass
+	
+func atkState(): #attacking
+	
+	pass
+
+func runState(): #running
+	
+	pass
+	
+func dodgeState(): #dodging
+	
+	pass
+	
+func moveState(): #movement
+	var motion = movedir.normalized() * moveSpeed
+	move_and_slide(motion, Vector2(0,0))
+	
+	
 	
 	pass
