7e5b68f42cb8cb2a049ce9d927d0c9c6f65f069e
envyniv <42321092+envyniv@users.noreply.github.com>
2020-07-17 15:09:11 +0200

    Update convert_audio.sh

--- convert_audio.sh
+++ convert_audio.sh
@@ -1,4 +1,4 @@
-
+#place files to convert in source_seets/OST/BGM or fx, depending on the type
 cd ./source_assets/OST/BGM
 for i in *.mp3; do ffmpeg -i "$i" -ar 48000 -vn -c:a libvorbis "${i%.*}.ogg"; done
 cd ../fx
