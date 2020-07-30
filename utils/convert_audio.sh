f93affab6b13c4187b974011dc98f6b329e4268c
envyniv <42321092+envyniv@users.noreply.github.com>
2020-07-30 15:37:24 +0200

    moved the project data into an ad-hoc folder

--- utils/convert_audio.sh
+++ utils/convert_audio.sh
@@ -1,5 +1,5 @@
 #place files to convert in source_seets/OST/BGM or fx, depending on the type
-cd ./source_assets/OST/BGM
+cd ../The Project/source_assets/OST/BGM
 for i in *.mp3; do ffmpeg -i "$i" -ar 48000 -vn -c:a libvorbis "${i%.*}.ogg"; done
 cd ../fx
 for i in *.mp3; do ffmpeg -i "$i" "${i%.*}.wav"; done
