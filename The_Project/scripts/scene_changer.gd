9ffc85088936acc7b1b7a15e0856bfc063a66716
envyniv <42321092+envyniv@users.noreply.github.com>
2020-09-16 15:20:11 +0200

    added godot font materials

---
+++ The_Project/scripts/scene_changer.gd
@@ -0,0 +1,16 @@
+extends Node
+
+signal scene_changed()
+
+onready var animation_player=$CanvasLayer/AnimationPlayer
+onready var color_rect=$CanvasLayer/Control/ColorRect
+
+func change_scene(path,delay) :
+	yield(get_tree().create_timer(delay),"timeout")
+	animation_player.play("fade")
+	yield(animation_player, "animation_finished")
+	assert(get_tree().change_scene(path) == OK)
+	animation_player.play_backwards("fade")
+	yield(animation_player, "animation_finished")
+	emit_signal("scene_changed")
+pass
