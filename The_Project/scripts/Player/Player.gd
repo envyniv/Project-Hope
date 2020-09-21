835ac394b3f259a8656b5191314d423fba878354
envyniv <42321092+envyniv@users.noreply.github.com>
2020-09-21 16:10:42 +0200

    initial commit to fix player movement

--- The_Project/scripts/Player/Player.gd
+++ The_Project/scripts/Player/Player.gd
@@ -9,7 +9,7 @@ onready var animPlayer=$Sprite/AnimationPlayer
 onready var animTree=$Sprite/AnimationPlayer/AnimationTree
 onready var animState=animTree.get("parameters/playback")
 #end_init
-	
+
 #state
 enum {
 	MOVE,
@@ -33,8 +33,6 @@ func _physics_process(_delta):
 	match state:
 		MOVE:
 			moveState();
-		RUN:
-			runState();
 		ATK:
 			atkState();
 		DODGE:
@@ -50,9 +48,12 @@ func inputChk(deactivate):
 	var DOWN = Input.is_action_pressed("ui_down")
 	var RIGHT = Input.is_action_pressed("ui_right")
 	var LEFT = Input.is_action_pressed("ui_left")
-	var ATTACK = Input.is_action_just_pressed("ui_attack") #so it is only done once
-	var DODGE = Input.is_action_just_pressed("ui_dodge")
-	var RUN = Input.is_action_pressed("ui_sprint")
+	var ATK_LIGHT = Input.is_action_just_pressed("ui_atklight")
+	var ATK_MED = Input.is_action_just_pressed("ui_atkmed")
+	var ATK_HEAVY = Input.is_action_just_pressed("ui_atkheavy")
+	var ATK_GOD = Input.is_action_just_pressed("ui_atkgod")
+	var DODGE_KEY = Input.is_action_just_pressed("ui_dodge")
+	var RUN_KEY = Input.is_action_pressed("ui_sprint")
 	movedir.x = -int(LEFT) + int(RIGHT) #don't move if the left and right keys are pressed
 	movedir.y = -int(UP) + int(DOWN)
 	if deactivate:
@@ -62,9 +63,11 @@ func inputChk(deactivate):
 
 
 func moveState(): #movement
+	inputChk(0)
 	var motion = movedir.normalized() * moveSpeed
 	motion = move_and_slide(motion, Vector2(0,0))
-	if RUN: state=RUN;
+	if RUN_KEY:
+		speed=9
 	pass
 
 
@@ -72,20 +75,13 @@ func moveState(): #movement
 func animHndlr():
 	if movedir!=Vector2.ZERO:
 		animTree.set("parameters/idle/blend_position", movedir)
-		animTree.set("parameters/atk/blend_position", movedir)
 		animTree.set("parameters/run/blend_position", movedir)
-		animTree.set("parameters/dodg/blend_position", movedir)
 		animTree.set("parameters/walk/blend_position", movedir)
 		match state:
 			MOVE:
 				animState.travel("walk")
 			RUN:
 				animState.travel("run")
-			ATK:
-				animState.travel("walk")
-			DODGE:
-				animState.travel("dodge")
-		
 	else:
 		animState.travel("idle")
 		pass
@@ -94,10 +90,6 @@ func animHndlr():
 
 func atkState(): #attacking
 	canDo = false
-	pass
-	
-func runState(): #running
-	speed = 6.5;
 	pass
 	
 func dodgeState(): #dodging
